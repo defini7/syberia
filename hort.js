0548aa7d0a29db4d0b11c9fe2521fce29f35659c
Лучший <75216201+defini7@users.noreply.github.com>
2022-02-18 15:57:42 +0300

    Delete hort.js

--- hort.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var interpreter_1 = require("./interpreter");
-var parser_1 = require("./parser");
-// tree is useless, but nodes are not
-var tree = (0, parser_1.parse)();
-for (var i = 0; i < parser_1.nodes.length; i++) {
-    console.log((0, interpreter_1.visit)(parser_1.nodes[i]));
-}
